510f7c9a4104b505037a04e94bc2b4b262a02277
Walner0 <walner-73@outlook.com>
2022-09-26 21:28:46 -0300

    1

---
+++ ExemploLuta/UltraEmojiCombat.java
@@ -0,0 +1,8 @@
+public class UltraEmojiCombat {
+    public static void main(String[] args) { 
+        Lutador l[] = new Lutador[6];
+
+        l[0] = new Lutador(31, 11, 2, 1, "PrettyBoy", "França", null, 1.75f, 68.9f);
+        l[0].apresentar();
+    }
+}
